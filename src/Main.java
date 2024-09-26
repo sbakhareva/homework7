47a1ad0ab39d3e17576319028e5ea8fddc72bc32
sbakhareva <snezhana.bakhareva@yandex.ru>
2024-09-26 18:33:20 +0300

    готовое дз??

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,105 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Task #1
+        System.out.println("Задача №1");
+        // можно оставить решение с процентами? а то копить очень долго)))))
+        int investment = 15000;
+        int savings = 0;
+        int month = 0;
+        while (savings < 2_459_000) {
+            savings = savings + savings / 100;
+            savings = savings + investment;
+            month++;
+            if (savings < 2459000) {
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + savings + " рублей.");
+            }
+        }
+
+        // Task #2
+        System.out.println("Задача №2");
+        int number = 1;
+        while (number <= 10) {
+            System.out.print(number + " ");
+            number++;
+        }
+        System.out.println();
+        for (number = 1; number <= 10; number++) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+
+        // Task #3
+        System.out.println("Задача №3");
+        int population = 12_000_000;
+        int year = 1;
+        int mortality = 8;
+        int birth = 17;
+        float populationGrowth = (float) (birth - mortality) / 1000;
+        while (year <= 10) {
+            System.out.println("Год " + year + ", численность населения составляет " + population);
+            population = (int) (population + population * populationGrowth);
+            year++;
+        }
+
+        // Task #4
+        System.out.println("Задача №4");
+        int monthlyPay = 15000;
+        float monthlyPercent = 0.07f;
+        int goal = 15000;
+        month = 1;
+        while (goal <= 12_000_000) {
+            System.out.println("Месяц " + month + ", сумма накоплений составляет " + goal);
+            goal = (int) (goal + goal * monthlyPercent);
+            goal = goal + monthlyPay;
+            month++;
+        }
+
+        // Task #5
+        System.out.println("Задача №5");
+        goal = 15000;
+        month = 1;
+        while (goal <= 12_000_000) {
+            goal = (int) (goal + goal * monthlyPercent);
+            goal = goal + monthlyPay;
+            month++;
+            if (month % 6 == 0) {
+                System.out.println("Месяц " + month + ", сумма накоплений составляет " + goal);
+            }
+        }
+
+        // Task #6
+        System.out.println("Задача №6");
+        goal = 15000;
+        month = 1;
+        while (month <= 117) {
+            goal = (int) (goal + goal * monthlyPercent);
+            goal = goal + monthlyPay;
+            month++;
+            if (month % 6 == 0) {
+                System.out.println("Месяц " + month + ", сумма накоплений составляет " + goal);
+            }
+        }
+
+        // Task #7
+        System.out.println("Задача №7");
+        int daysInMonth = 31;
+        int friday = 1;
+        while (friday < daysInMonth) {
+                System.out.println("Сегодня пятница, " + friday + "-ое число. Необходимо подготовить отчет.");
+                friday += 7;
+        }
+        // Task #8
+        System.out.println("Задача №8");
+        int currentYear = 2024;
+        int yearOfAppearance = 0;
+        int lastAppearance = currentYear - 200;
+        int nextAppearance = currentYear + 100;
+        while (yearOfAppearance >= 0) {
+            yearOfAppearance += 79;
+            if (yearOfAppearance > lastAppearance && yearOfAppearance < nextAppearance) {
+                System.out.println(yearOfAppearance);
+            }
         }
     }
 }
