5e58c6576c4492101816750d2e096c8b1c6d6cb3
sbakhareva <snezhana.bakhareva@yandex.ru>
2024-09-27 08:24:47 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,7 @@ public class Main {
             number++;
         }
         System.out.println();
-        for (number = 1; number <= 10; number++) {
+        for (number = 10; number >= 1; number--) {
             System.out.print(number + " ");
         }
         System.out.println();
